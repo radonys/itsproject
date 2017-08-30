f3f37178a2a69e24bc00aac7538556d1c972a7bd
Vaishnav Murali <vaishnavm217>
2017-08-31 00:32:56 +0530

    some commit

--- restapi/urls.py
+++ restapi/urls.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'', views.index,name="index"),
+    url(r'^index', views.index,name="index"),
+	url(r'^households/', views.funchid,name="households"),
+	url(r'^farms/', views.farms,name="farms"),
+	url(r'^photos/', views.photos,name="photos"),
+	url(r'^members/', views.members,name="members"),
+	url(r'^admin/', admin.site.urls),
 ]
--- restapi/views.py
+++ restapi/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
+import requests
 # Create your views here.
 def index(request):
 	return render(request,"restapi/index.html")
+def funchid(request):
+	r=False
+	if request.POST:
+		h=request.POST['hid']
+		data = requests.post('https://farmereverywhere-server.herokuapp.com/households',data={"HID":h})
+		r = True
+		return render(request,'restapi/households.html',{ "r": r ,"data":data,"h":h })
+	else:
+		return render(request,'restapi/households.html',{ "r": r })
+def members(request):
+	r=False
+	if request.POST:
+		h=request.POST['hid']
+		data = requests.post('https://farmereverywhere-server.herokuapp.com/members',data={"HID":h})
+		r = True
+		return render(request,'restapi/members.html',{ "r": r ,"data":data,"h":h })
+	else:
+		return render(request,'restapi/members.html',{ "r": r })
+def farms(request):
+	r=False
+	if request.POST:
+		h=request.POST['hid']
+		data = requests.post('https://farmereverywhere-server.herokuapp.com/farms',data={"HID":h})
+		r = True
+		return render(request,'restapi/farms.html',{ "r": r ,"data":data,"h":h })
+	else:
+		return render(request,'restapi/farms.html',{ "r": r })
+def photos(request):
+	r=False
+	if request.POST:
+		h=request.POST['hid']
+		data = requests.post('https://farmereverywhere-server.herokuapp.com/photos',data={"HID":h})
+		r = True
+		return render(request,'restapi/photos.html',{ "r": r ,"data":data,"h":h })
+	else:
+		return render(request,'restapi/photos.html',{ "r": r })
+		
 	
